7af69c915867762faa87be2ac4b6ce0a524d22c7
David Nienge <davidnienge@gmail.com>
2025-05-04 21:56:25 +0100

    typos

--- data/admission.ts
+++ data/admission.ts
@@ -1,6 +1,6 @@
 export const admissionDetails = {
   title: "Apply for Admission",
-  bullet: "2025 admissions will be opened by August",
+  bullet: "2025 admissions will be opened by August.",
   description:
     "We are  redefining education by a providing a dynamic learning environment that goes beyond traditional academics to 21st century learning raising problem solvers who will shape the future.",
   btnText: "Join the waitlist",
